20cc45caf35945381d5b4fc5d83dc92c0088fa30
Даниил Кустов <dkustov4@gmail.com>
2021-09-03 18:26:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,229 @@
+# HHelper (x86)
+
+A single header signal library
+
+## Usage
+
+Simply include ktsignal.hpp. C++17 compatible compiler required
+
+## Examples
+
+### Basics
+
+The following example shows how to connect and emit a signal
+
+```cpp
+void on_click(int value) { }
+
+class A {
+public:
+    void on_class_click(int value) { }
+};
+
+int main() {
+    // Signal with function signature in template parameter
+    ktsignal::ktsignal<void(int)> click{};
+
+    // Connect to a function callback
+    click.connect(on_click);
+    
+    // Connect to a member function callback
+    A object;
+    click.connect(&object, &A::on_class_click);
+
+    // Connect to a lambda callback
+    click.connect([](int){ });
+
+    // Signal emit
+    click.emit(1);
+}
+```
+
+Scoped connections automatically disconnect in the end of scope
+
+```cpp
+void on_click(int value) { }
+
+class A {
+public:
+    void on_class_click(int value) { }
+};
+
+int main() {
+    ktsignal::ktsignal<void(int)> click{};
+    
+    {
+        auto connection = click.scoped_connect(on_click);
+        A object;
+        auto method_connection = click.scoped_connect(&object, &A::on_class_click);
+
+        click.scoped_connect([](int v){ std::cout << "This will never be printed" << std::endl; });
+
+        // on_click and on_class_click are called
+        click.emit(1);
+    }
+    // nothing is called
+    click.emit(1);
+}
+```
+
+### Connectins / Disconnecting callbacks
+
+You can easily disconnect slot from Signal
+
+```cpp
+void on_click(int value) { }
+
+class A {
+public:
+    void on_class_click(int value) { }
+};
+
+int main() {
+    ktsignal::ktsignal<void(int)> click{};
+
+    // Save connection
+    auto connection = click.connect(on_click);
+
+    // Connect to a lambda callback
+    click.connect([](int){ });
+
+    // Signal emit (on_click and lambda are called)
+    click.emit(1);
+
+    // Disconnect on_click callback from signal
+    connection.disconnect();
+
+    // Only lambda will be called
+    click.emit(1);
+}
+```
+
+A few important notes about `ktsignal_connection` object
+- `ktsignal_connection` is default-constructible, moveable but not copyable.
+- You must make sure that the connection will not be used after the signal is destroyed.
+
+### Iterating through signal slots
+
+You can easily iterate over signal slots via range-based for loop and get slot return value
+
+```cpp
+int on_click(int value) { return 5; }
+int on_click_second(int value) { return 1; }
+
+int main() {
+    ktsignal::ktsignal<int(int)> click{};
+
+    click.connect(on_click);
+    click.connect(on_click_second);
+
+    // Will display `emit_iterate returned 5 emit_iterate returned 1`
+    for (auto returned : signal.emit_iterate(1)) {
+        std::cout << "emit_iterate returned " << returned << " ";
+    }
+}
+```
+
+Also you can use functions from the stdlib
+
+```cpp
+int on_click(int value) { return 5; }
+int on_click_second(int value) { return 1; }
+
+int main() {
+    ktsignal::ktsignal<int(int)> click{};
+
+    click.connect(on_click);
+    click.connect(on_click_second);
+
+    auto iterate = signal.emit_iterate(0);
+    auto accumulated = std::accumulate(iterate.begin(), iterate.end(), 0);
+
+    // Will display 6
+    std::cout << "Accumulated: " << accumulated << std::endl;
+}
+```
+
+### Using `ktsignal` in multithreaded code
+
+For multi-threaded use, you should use `ktsignal_threadsafe`
+
+```cpp
+void func_thread(int v) {
+    std::cout << "before sleep" << std::endl;
+    std::this_thread::sleep_for(1000ms);
+    std::cout << "after sleep" << std::endl;
+    return 2;
+}
+
+int main() {
+    ktsignal::ktsignal_threadsafe_emit<int(std::string&)> signal{};
+
+    signal.connect(func_thread);
+
+    // Create a thread that emit immediately
+    std::thread([&signal]() {
+
+        // Create a thread that emit after 100ms
+        std::thread(
+            [&signal]() {
+                std::this_thread::sleep_for(100ms);
+                signal.emit(1);
+            }
+        ).detach();
+
+         signal.emit(2);
+    }).join();
+
+    std::this_thread::sleep_for(1.5s);
+}
+```
+Output:
+[func_thread] - before sleep
+[func_thread] - before sleep
+[func_thread] - after sleep
+[func_thread] - after sleep
+```
+
+```
+
+You can also use the `ktsignal_threadsafe_emit` when you need to make the emit function blocking.
+
+```cpp
+void func_thread(int v) {
+    std::cout << "before sleep" << std::endl;
+    std::this_thread::sleep_for(1000ms);
+    std::cout << "after sleep" << std::endl;
+    return 2;
+}
+
+int main() {
+    ktsignal::ktsignal_threadsafe_emit<int(std::string&)> signal{};
+
+    signal.connect(func_thread);
+
+    // Create a thread that emit immediately
+    std::thread([&signal]() {
+
+        // Create a thread that emit after 100ms
+        std::thread(
+            [&signal]() {
+                std::this_thread::sleep_for(100ms);
+                signal.emit(1);
+            }
+        ).detach();
+
+         signal.emit(2);
+    }).join();
+
+    std::this_thread::sleep_for(1.5s);
+}
+```
+
+```
+Output:
+[func_thread] - before sleep
+[func_thread] - after sleep
+[func_thread] - before sleep
+[func_thread] - after sleep
+```
---
+++ include/ktsignal.hpp
@@ -0,0 +1,182 @@
+#ifndef KTSIGNAL_HPP
+#define KTSIGNAL_HPP
+
+#include <functional>
+#include <list>
+#include <shared_mutex>
+#include <mutex>
+#include <tuple>
+
+namespace ktsignal {
+    struct empty_mutex {
+        void lock() {}
+        void unlock() {}
+
+        void lock_shared() {}
+        void unlock_shared() {}
+    };
+    template <typename FuncSig, bool thread_safe = true, bool emit_thread_safe = false>
+    class ktsignal_impl
+    {
+        using mutex_type = std::conditional_t<thread_safe, std::shared_mutex, empty_mutex>;
+        using emit_locker = std::conditional_t<emit_thread_safe, std::unique_lock<mutex_type>, std::shared_lock<mutex_type>>;
+    public:
+        using slot_type = std::function<FuncSig>;
+        using slot_container = std::list<slot_type>;
+        using slot_iterator = typename slot_container::iterator;
+
+        class ktsignal_connection
+        {
+        public:
+            void disconnect() {
+                erase_func();
+                erase_func = []() {};
+            }
+            ktsignal_connection(std::function<void()> func) : erase_func(func) {};
+            ktsignal_connection(ktsignal_connection&) = delete;
+            ktsignal_connection(ktsignal_connection&&) = default;
+        private:
+            std::function<void()> erase_func;
+        };
+
+        class ktsignal_scoped_connection : public ktsignal_connection
+        {
+        public:
+            ~ktsignal_scoped_connection() { ktsignal_connection::disconnect(); }
+            ktsignal_scoped_connection(std::function<void()> func) : ktsignal_connection(func) {};
+            ktsignal_scoped_connection(ktsignal_scoped_connection&) = delete;
+            ktsignal_scoped_connection(ktsignal_scoped_connection&& Right) = default;
+        };
+
+        ktsignal_connection connect(slot_type slot) {
+            std::unique_lock<mutex_type> lock(slots_mutex);
+
+            auto erase_iter = slots_.emplace(slots_.end(), std::move(slot));
+
+            std::function<void()> lambda = [&slots_mutex = this->slots_mutex, &slots_ = this->slots_, erase_iter]() -> void {
+                std::unique_lock<mutex_type> lock(slots_mutex);
+                slots_.erase(erase_iter);
+            };
+            return ktsignal_connection{ std::move(lambda) };
+        }
+        ktsignal_scoped_connection scoped_connect(slot_type slot) {
+            std::unique_lock<mutex_type> lock(slots_mutex);
+
+            auto erase_iter = slots_.emplace(slots_.end(), std::move(slot));
+
+            std::function<void()> lambda = [&slots_mutex = this->slots_mutex, &slots_ = this->slots_, erase_iter]() -> void {
+                std::unique_lock<mutex_type> lock(slots_mutex);
+                slots_.erase(erase_iter);
+            };
+            return ktsignal_scoped_connection{ std::move(lambda) };
+        }
+
+        template <class C, typename R, typename... Args>
+        ktsignal_connection connect(C* ptr, R(C::* func)(Args...)) {
+            return connect([ptr, func](Args&&... args) -> R { return (ptr->*func)(std::forward<Args>(args)...); });
+        }
+        template <class C, typename R, typename... Args>
+        ktsignal_scoped_connection scoped_connect(C* ptr, R(C::* func)(Args...)) {
+            return scoped_connect([ptr, func](Args&&... args) -> R { return (ptr->*func)(std::forward<Args>(args)...); });
+        }
+
+        template <typename... Args>
+        void emit(Args&&... args) {
+            emit_locker lock(slots_mutex);
+            for (auto& f : slots_) {
+                f(std::forward<Args>(args)...);
+            }
+        }
+
+        template <typename... CallArgs>
+        class signal_iterator {
+
+#if __cpp_lib_apply < 201603L || 1
+            template <int... Is>
+            struct index {};
+
+            template <int N, int... Is>
+            struct gen_seq : gen_seq<N - 1, N - 1, Is...> {};
+
+            template <int... Is>
+            struct gen_seq<0, Is...> : index<Is...> {};
+
+            template <typename... Ts, int... Is>
+            auto iter_call(slot_type slot, std::tuple<Ts...>& tup, index<Is...>)
+            {
+                return slot(std::get<Is>(tup)...);
+            }
+
+            template <typename... Ts>
+            auto iter_call(slot_type slot, std::tuple<Ts...>& tup)
+            {
+                return iter_call(slot, tup, gen_seq<sizeof...(Ts)>{});
+            }
+#endif
+
+        public:
+            using difference_type = std::ptrdiff_t;
+            using value_type = typename slot_type::result_type;
+
+            signal_iterator(slot_iterator iter, mutex_type& slots_mutex, std::tuple<CallArgs&&...>& tup) :
+                slots_mutex_(slots_mutex), iter_(iter),
+                stored_args(tup) {}
+
+            value_type operator*() {
+                emit_locker lock(slots_mutex_);
+#if __cpp_lib_apply >= 201603L && 0
+                return std::apply(*iter_, stored_args);
+#else
+                return iter_call(*iter_, stored_args);
+#endif
+            }
+
+            signal_iterator& operator++() { iter_++; return *this; }
+
+            signal_iterator operator++(int) { signal_iterator tmp = *this; ++(*this); return tmp; }
+
+            friend bool operator== (const signal_iterator& a, const signal_iterator& b) { return a.iter_ == b.iter_; }
+            friend bool operator!= (const signal_iterator& a, const signal_iterator& b) { return a.iter_ != b.iter_; }
+        private:
+            slot_iterator iter_;
+            mutex_type& slots_mutex_;
+            std::tuple<CallArgs&&...>& stored_args;
+        };
+
+        template <typename... CallArgs>
+        class iterate {
+        public:
+            iterate(std::list<slot_type>& slots, mutex_type& slots_mutex, CallArgs&&... args) :
+                slots_(slots), slots_mutex_(slots_mutex), stored_args(std::forward<CallArgs>(args)...) {}
+
+            auto begin() {
+                return signal_iterator<CallArgs...>(slots_.begin(), slots_mutex_, stored_args);
+            }
+            auto end() {
+                return signal_iterator<CallArgs...>(slots_.end(), slots_mutex_, stored_args);
+            }
+        private:
+            std::tuple<CallArgs&&...> stored_args;
+            std::list<slot_type>& slots_;
+            mutex_type& slots_mutex_;
+        };
+
+        template <typename... Args>
+        iterate<Args...> emit_iterate(Args&&... args) {
+            return iterate<Args...>(slots_, slots_mutex, std::forward<Args>(args)...);
+        }
+
+    private:
+        std::list<slot_type> slots_;
+        mutex_type slots_mutex;
+    };
+
+    template <typename FuncSig>
+    using ktsignal = ktsignal_impl<FuncSig, false, false>;
+    template <typename FuncSig>
+    using ktsignal_threadsafe = ktsignal_impl<FuncSig, true, false>;
+    template <typename FuncSig>
+    using ktsignal_threadsafe_emit = ktsignal_impl<FuncSig, true, true>;
+}
+
+#endif // KTSIGNAL_HPP
